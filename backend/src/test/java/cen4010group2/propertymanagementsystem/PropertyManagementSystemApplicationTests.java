be7f3821a1ac247ef1b8b8c4bd1ff00ab85d3b06
drakeum <pb26413799@live.com>
2022-10-28 01:14:59 -0400

    Initial Commit

---
+++ backend/src/test/java/cen4010group2/propertymanagementsystem/PropertyManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package cen4010group2.propertymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementSystemApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
